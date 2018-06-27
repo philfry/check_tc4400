a0af4215b95bd22b412f7a474fd3ed175254d009
Philippe Kueck <bqobccy6ejnq2bqvmebqiwqha4cs4@protected32.unixadm.org>
2018-06-27 12:17:51 +0200

    initial checkin

---
+++ README.md
@@ -0,0 +1,56 @@
+# NAME
+
+check\_tc4400
+
+# VERSION
+
+0.1
+
+# SYNOPSIS
+
+```
+check_tc4400 -H HOST -u USER -p PASS
+```
+
+# OPTIONS
+
+- **H** _HOSTNAME_
+
+    Hostname or ip address of your tc4400 modem.
+
+- **u** _username_
+
+    Username to authenticate with to the modem's webinterface.
+
+- **p** _password_
+
+    Password to authenticate with to the modem's webinterface.
+
+# DESCRIPTION
+
+This script connects to the TC4400 webinterface and parses the connection status page.
+
+It warns or returns a critical state if:
+
+```
+* Connectivity State is not "OK"
+* Boot State is not "OK"
+* Configuration File is not "OK"
+* Security is not "Enabled"
+* downstream channel is not "Locked"
+* downstream channel signal/noise ratio is below 32dB/30dB (QAM256)
+* downstream channel signal/noise ratio is below 24dB/26dB (QAM64)
+* downstream channel receive level is below -8dBmV/-6dBmV or above 18.1dBmV/20.1dBmV
+* upstream channel transmission level is below 32dBmV/34dBmV or above 54.1dBmV/56.1dBmV
+```
+
+# DEPENDENCIES
+
+- `LWP::UserAgent`
+- `HTML::TableExtract`
+- `Pod::Usage`
+- `Getopt::Long`
+
+# AUTHOR
+
+Philippe Kueck &lt;projects at unixadm dot org>
---
+++ check_tc4400.php
@@ -0,0 +1,76 @@
+<?php
+/*
+ *
+ * pnp4nagios template for check_tc4400.
+ *
+ * use RRD_STORAGE_TYPE = MULTIPLE.
+ *
+ * Author: Philippe Kueck <projects at unixadm dot org>
+ *
+ */
+
+$myds = array();
+for ($i = 1; $i <= count($DS); $i++) {$myds[$NAME[$i]] = $i;}
+
+// ==== upstream transmision level ====
+$i++;
+$ds_name[$i] = "us_tlvl";
+$opt[$i] = " --vertical-label dBmV -E -b 1000 --title \"Upstream transmission level\" ";
+// limits for QAM256
+$def[$i]  = rrd::line(1, "32", "#ff0000", ":dashes=5");
+$def[$i] .= rrd::line(1, "34", "#e0e000", ":dashes=5");
+$def[$i] .= rrd::line(1, "54.1", "#e0e000", ":dashes=5");
+$def[$i] .= rrd::line(1, "56.1", "#ff0000", ":dashes=5");
+for ($c = 1; $c <= 5; $c++) {
+    $v = sprintf("up%02d_tlvl", $c);
+    $def[$i] .= rrd::def($v, $RRDFILE[$myds[$v]], $DS[$myds[$v]], "AVERAGE");
+    $def[$i] .= rrd::line1($v, rrd::color($c), rrd::cut("channel $c", 24));
+    $def[$i] .= rrd::gprint($v, array("LAST", "AVERAGE", "MAX"), "%3.1lf dBmV");
+}
+
+// ==== downstream SNR ====
+$i = 0;
+$ds_name[$i] = "ds_snr";
+$opt[$i] = " --vertical-label dB -E -b 1000 --title \"Downstream SNR\" ";
+
+$def[$i] = rrd::hrule("32", "#e0e000", ":dashes=5");
+$def[$i] .= rrd::hrule("30", "#ff0000", ":dashes=5");
+for ($c = 1; $c <= 31; $c++) {
+    $v = sprintf("dn%02d_snr", $c);
+    $def[$i] .= rrd::def($v, $RRDFILE[$myds[$v]], $DS[$myds[$v]], "AVERAGE");
+    $def[$i] .= rrd::line1($v, rrd::color($c), rrd::cut("channel $c", 24));
+    $def[$i] .= rrd::gprint($v, array("LAST", "AVERAGE", "MAX"), "%3.1lf dB");
+}
+
+// ==== downstream receive level ====
+$i++;
+$ds_name[$i] = "ds_rlvl";
+$opt[$i] = " --vertical-label dBmV -E -b 1000 --title \"Downstream receive level\" ";
+$def[$i]  = rrd::line(1, "-8", "#ff0000", ":dashes=5");
+$def[$i] .= rrd::line(1, "-6", "#e0e000", ":dashes=5");
+$def[$i] .= rrd::line(1, "18.1", "#e0e000", ":dashes=5");
+$def[$i] .= rrd::line(1, "20.1", "#ff0000", ":dashes=5");
+for ($c = 1; $c <= 31; $c++) {
+    $v = sprintf("dn%02d_rlvl", $c);
+    $def[$i] .= rrd::def($v, $RRDFILE[$myds[$v]], $DS[$myds[$v]], "AVERAGE");
+    $def[$i] .= rrd::line1($v, rrd::color($c), rrd::cut("channel $c", 24));
+    $def[$i] .= rrd::gprint($v, array("LAST", "AVERAGE", "MAX"), "%3.1lf dBmV");
+}
+
+// ==== downstream codewords ====
+foreach (array(
+    'cwpass' => 'unerrored',
+    'cwcorr' => 'corrected',
+    'cwfail' => 'uncorrectable'
+) as $id => $title) {
+    $i++;
+    $ds_name[$i] = "ds_$id";
+    $opt[$i] = " -E -b 1000 --title \"Downstream $title codewords\" ";
+    $def[$i] = "";
+    for ($c = 1; $c <= 31; $c++) {
+    	$v = sprintf("dn%02d_%s", $c, $id);
+    	$def[$i] .= rrd::def($v, $RRDFILE[$myds[$v]], $DS[$myds[$v]], "AVERAGE");
+    	$def[$i] .= rrd::line1($v, rrd::color($c), rrd::cut("channel $c", 24));
+    	$def[$i] .= rrd::gprint($v, array("LAST", "AVERAGE", "MAX"), "%7.0lf");
+    }
+}
