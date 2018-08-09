88f479e2d615c57ae5849ca3c500a7399a63f41c
Philippe Kueck <bqobccy6ejnq2bqvmebqiwqha4cs4@protected32.unixadm.org>
2018-08-09 22:19:12 +0200

    do not hardcode channel limits

--- check_tc4400.php
+++ check_tc4400.php
@@ -13,29 +13,31 @@ $myds = array();
 for ($i = 1; $i <= count($DS); $i++) {$myds[$NAME[$i]] = $i;}
 
 // ==== upstream transmision level ====
-$i++;
+$i = 0;
 $ds_name[$i] = "us_tlvl";
 $opt[$i] = " --vertical-label dBmV -E -b 1000 --title \"Upstream transmission level\" ";
 $def[$i]  = rrd::line(1, "32", "#ff0000", ":dashes=5");
 $def[$i] .= rrd::line(1, "34", "#e0e000", ":dashes=5");
 $def[$i] .= rrd::line(1, "54.1", "#e0e000", ":dashes=5");
 $def[$i] .= rrd::line(1, "56.1", "#ff0000", ":dashes=5");
-for ($c = 1; $c <= 5; $c++) {
+for ($c = 1;; $c++) {
     $v = sprintf("up%02d_tlvl", $c);
+    if (!isset($myds[$v])) break;
     $def[$i] .= rrd::def($v, $RRDFILE[$myds[$v]], $DS[$myds[$v]], "AVERAGE");
     $def[$i] .= rrd::line1($v, rrd::color($c), rrd::cut("channel $c", 24));
     $def[$i] .= rrd::gprint($v, array("LAST", "AVERAGE", "MIN", "MAX"), "%3.1lf dBmV");
 }
 
 // ==== downstream SNR ====
-$i = 0;
+$i++;
 $ds_name[$i] = "ds_snr";
 $opt[$i] = " --vertical-label dB -E -b 1000 --title \"Downstream SNR\" ";
 // limits for QAM256
 $def[$i] = rrd::hrule("32", "#e0e000", ":dashes=5");
 $def[$i] .= rrd::hrule("30", "#ff0000", ":dashes=5");
-for ($c = 1; $c <= 31; $c++) {
+for ($c = 1;; $c++) {
     $v = sprintf("dn%02d_snr", $c);
+    if (!isset($myds[$v])) break;
     $def[$i] .= rrd::def($v, $RRDFILE[$myds[$v]], $DS[$myds[$v]], "AVERAGE");
     $def[$i] .= rrd::line1($v, rrd::color($c), rrd::cut("channel $c", 24));
     $def[$i] .= rrd::gprint($v, array("LAST", "AVERAGE", "MIN"), "%3.1lf dB");
@@ -49,8 +51,9 @@ $def[$i]  = rrd::line(1, "-8", "#ff0000", ":dashes=5");
 $def[$i] .= rrd::line(1, "-6", "#e0e000", ":dashes=5");
 $def[$i] .= rrd::line(1, "18.1", "#e0e000", ":dashes=5");
 $def[$i] .= rrd::line(1, "20.1", "#ff0000", ":dashes=5");
-for ($c = 1; $c <= 31; $c++) {
+for ($c = 1;; $c++) {
     $v = sprintf("dn%02d_rlvl", $c);
+    if (!isset($myds[$v])) break;
     $def[$i] .= rrd::def($v, $RRDFILE[$myds[$v]], $DS[$myds[$v]], "AVERAGE");
     $def[$i] .= rrd::line1($v, rrd::color($c), rrd::cut("channel $c", 24));
     $def[$i] .= rrd::gprint($v, array("LAST", "AVERAGE", "MIN", "MAX"), "%3.1lf dBmV");
@@ -66,8 +69,9 @@ foreach (array(
     $ds_name[$i] = "ds_$id";
     $opt[$i] = " -E -b 1000 --title \"Downstream $title codewords\" ";
     $def[$i] = "";
-    for ($c = 1; $c <= 31; $c++) {
+    for ($c = 1;; $c++) {
         $v = sprintf("dn%02d_%s", $c, $id);
+        if (!isset($myds[$v])) break;
         $def[$i] .= rrd::def($v, $RRDFILE[$myds[$v]], $DS[$myds[$v]], "AVERAGE");
         $def[$i] .= rrd::area($v, rrd::color($c), rrd::cut("channel $c", 24), "STACK");
         $def[$i] .= rrd::gprint($v, array("LAST", "AVERAGE", "MAX"), "%7.0lf");
